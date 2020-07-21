fa58f65d4863e78a0fce55906291e84f3ce0f5ad
=Munna Bhakta <=munna.bhakta1001@gmail.com>
2020-07-22 05:01:57 +0530

    Updates

---
+++ precache-manifest.3eb7d174d8f0c86abdfa1f1bfee97f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4cfd888f2a9aba3db9446f79a53a432",
+    "url": "/munnabhakta/index.html"
+  },
+  {
+    "revision": "910b484494435f586855",
+    "url": "/munnabhakta/static/css/main.ae574413.chunk.css"
+  },
+  {
+    "revision": "7b3927c71e2f854fa37e",
+    "url": "/munnabhakta/static/js/2.b23c80e5.chunk.js"
+  },
+  {
+    "revision": "910b484494435f586855",
+    "url": "/munnabhakta/static/js/main.11d11e76.chunk.js"
+  },
+  {
+    "revision": "9bd40e5f8723270eb1c4",
+    "url": "/munnabhakta/static/js/runtime~main.c11b4397.js"
+  },
+  {
+    "revision": "3677f872501c7bebeadb061b3cd7ac5e",
+    "url": "/munnabhakta/static/media/background.3677f872.jpg"
+  }
+]);
