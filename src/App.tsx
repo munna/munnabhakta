e6ddccf88fc1188e6b60d0e5de4ae93f60f7aaae
=Munna Bhakta <=munna.bhakta1001@gmail.com>
2020-07-26 10:04:47 +0530

    updated responsive with imporved content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { SocialIcon } from 'react-social-icons';
 import logo from './assets/logo.jpg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 const App: React.FC = () => {
   return (
@@ -12,9 +12,29 @@ const App: React.FC = () => {
   and took admission in NIIT and started my programming carrier. While I was stuyding, I started my carrier in Torush InfoTech PVT. LTD. in 2008.
 </p>
 <p>Currently working as Senior Project Manager at Fervent Software Solutions - a Software Development company based out from New Delhi, India</p>
-<p><SocialIcon
-  url="https://twitter.com/munnabhakta"
-/></p>
+<p>Email: munna.bhakta1001@gmail.com</p>
+<p>
+<a href="https://twitter.com/munnabhakta" target="_blank">
+  <span style={{fontSize:'30px',color:'#0383f5fc'}}>
+    <i className="fa fa-twitter"></i>
+</span>
+</a> 
+<a href="https://www.facebook.com/munna.bhakta" target="_blank">
+<span style={{fontSize:'30px',color:'blue', marginRight:'10px'}}>
+<i className="fa fa-facebook"></i>
+</span>
+</a>
+<a href="https://github.com/munna" target="_blank">
+<span style={{fontSize:'30px',color:'#FFF', marginRight:'10px'}}>
+<i className="fa fa-github"></i>
+</span>
+</a>
+<a href="https://in.linkedin.com/in/munna-bhakta-b762a334" target="_blank">
+<span style={{fontSize:'30px',color:'#FFF', marginRight:'10px'}}>
+<i className="fa fa-linkedin"></i>
+</span>
+</a>
+</p>
       </div>
       <div className="col-3">
       <div className="profile-box">
@@ -36,7 +56,7 @@ const App: React.FC = () => {
         </progress><div className="value">90%</div></div>
       <div className="technology-item"><div className="title">PL\SQL</div><progress max="100" value="70" style={{width:'100%'}}>
         </progress><div className="value">70%</div></div>
-      <div className="technology-item"><div className="title">ArcGIS</div><progress max="100" value="80" style={{width:'100%'}}>
+      <div className="technology-item"><div className="title">ArcGIS JS</div><progress max="100" value="80" style={{width:'100%'}}>
         </progress><div className="value">80%</div></div>
         <div className="technology-item"><div className="title">ArcGIS Server</div><progress max="100" value="50" style={{width:'100%'}}>
         </progress><div className="value">50%</div></div>  
